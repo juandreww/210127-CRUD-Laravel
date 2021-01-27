1192d1c4b596281ded743b9bfb5d49b835528b0a
Juan Andrew Sasongko <juandreww@outlook.com>
2021-01-27 22:10:42 +0700

    Create AccAccountController.php

---
+++ AccAccountController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AccAccountController extends Controller
+{
+    public function index()
+    {
+    	// mengambil data dari table pegawai
+    	$accaccount = DB::table('accaccount')->get();
+ 
+    	// mengirim data pegawai ke view index
+        return view('index',['accaccount' => $accaccount]);
+    }
+}
